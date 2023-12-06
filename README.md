364e811c0c461ea1582c51b5bf503dbff0b4878e
Baptiste Fontaine <baptiste@bixoto.com>
2023-12-06 09:41:36 +0000

    refactor + add doc

--- README.md
+++ README.md
@@ -3,4 +3,31 @@
 **mmc** is a **m**inimal **M**attermost **c**lient for Python. It calls the v4 API endpoints and is meant for basic
 uses.
 
+## Install
 
+    pip install mmc
+
+## Usage
+
+```python
+from mmc import Mattermost
+
+m = Mattermost(
+    "chat.example.com",
+    access_token="...",
+    team_id="...",
+    team_slug="my-team",
+)
+
+print("Teams:")
+for team in m.get_teams():
+    print(f"* {team['display_name']}")
+
+print("Users:")
+for user in m.get_users():
+    print(f"* {user['username']}")
+
+for post in m.get_channel_posts(channel_id="..."):
+    print(post["message"])
+
+```
--- mmc/__init__.py
+++ mmc/__init__.py
@@ -1,62 +1,83 @@
-from typing import List, Iterable, Optional
+from typing import List, Iterable, Tuple
 
-from api_session import APISession
+from api_session import APISession, JSONDict
+
+__all__ = ["Mattermost"]
+
+
+def ordered_items(response: JSONDict, items_keys: str) -> List[Tuple[str, JSONDict]]:
+    return [
+        (item_id, response[items_keys][item_id])
+        for item_id in response["order"]
+    ]
 
 
 class Mattermost(APISession):
-    def __init__(self, domain: str, access_token: str, *, team_id: str, team_slug: str):
-        super().__init__(
-            base_url=f"https://{domain}/api/v4",
-        )
+    def __init__(self, domain: str, *, access_token: str, https=True, **kwargs):
+        protocol = "https" if https else "http"
+        self.public_url = f"{protocol}://{domain}"
+
+        super().__init__(base_url=f"{self.public_url}/api/v4", **kwargs)
         self.headers["Authorization"] = f"Bearer {access_token}"
         self.domain = domain
-        self.team_id = team_id
-        self.team_slug = team_slug
 
-    def post_link(self, post_id: str):
-        return f"https://{self.domain}/{self.team_slug}/pl/{post_id}"
+    def get_post_url(self, team_slug: str, post_id: str):
+        """Return a post URL given the team slug and the post ID."""
+        return f"{self.public_url}/{team_slug}/pl/{post_id}"
 
-    def get_teams(self):
-        return self.get_json_api("/teams")
-
-    def get_channels(self):
-        return self.get_json_api("/channels")
-
-    def get_users(self):
-        return self.get_json_api("/users")
-
-    def get_bots(self):
-        return self.get_json_api("/bots")
-
-    def get_channel_posts(self, channel_id: str):
-        before = ""
-        per_page = 100
+    def _get_paginated_entities(self, endpoint: str, **params):
+        params = dict(params)
+        page = params.get("page", 0)
         while True:
-            resp = self.get_json_api(f"/channels/{channel_id}/posts?per_page={per_page}&before={before}")
-
-            if not resp["order"]:
+            if entities := self.get_json_api(endpoint,
+                                             params={**params, "page": page}):
+                yield from entities
+                page += 1
+            else:
                 return
 
-            for post_id in resp["order"]:
-                yield resp["posts"][post_id]
-                before = post_id
+    def get_teams(self) -> Iterable[JSONDict]:
+        """Get all teams."""
+        return self._get_paginated_entities("/teams")
 
-    def get_files(self, terms: str):
-        return self.post_api(f"/teams/{self.team_id}/files/search", json={"terms": terms, "is_or_search": False}).json()
+    def get_channels(self, **kwargs) -> Iterable[JSONDict]:
+        """Get all channels."""
+        return self._get_paginated_entities("/channels", **kwargs)
 
-    def iter_all_files(self, extensions: Optional[Iterable[str]] = None):
-        if extensions is None:
-            # Didn't find a way to query all files at once
-            extensions = [
-                "pdf", "zip", "7z", "gz", "bz2", "tar", "gzip", "jpeg", "jpg", "tiff", "gif", "png", "docx", "doc",
-                "xls", "xlsx", "webp", "webm", "mp4", "mp3", "avi", "txt", "json", "jsons", "csv", "tsv",
-            ]
-        for extension in extensions:
-            payload = self.get_files(f"ext:{extension}")
-            yield from payload["file_infos"].values()
+    def get_users(self, **kwargs) -> Iterable[JSONDict]:
+        """Get all users."""
+        return self._get_paginated_entities("/users", **kwargs)
 
-    def get_posts_by_ids(self, ids: List[str]):
+    def get_bots(self, **kwargs) -> Iterable[JSONDict]:
+        """Get all bots."""
+        return self._get_paginated_entities("/bots", **kwargs)
+
+    def get_channel_posts(self, channel_id: str, *, per_page=100, before="") -> Iterable[JSONDict]:
+        """Get all posts from a channel."""
+        while True:
+            resp = self.get_json_api(f"/channels/{channel_id}/posts",
+                                     params={"per_page": per_page, "before": before})
+            if posts := ordered_items(resp, "posts"):
+                for post_id, post in posts:
+                    yield post
+                    before = post_id
+            else:
+                return
+
+    def get_team_files(self, team_id: str, terms: str) -> Iterable[JSONDict]:
+        """Get files matching search terms."""
+        resp = self.post_api(f"/teams/{team_id}/files/search", json={
+            "terms": terms,
+            "is_or_search": False,
+        }).json()
+        return [
+            file for _, file in ordered_items(resp, "file_infos")
+        ]
+
+    def get_posts_by_ids(self, ids: List[str]) -> Iterable[JSONDict]:
+        """Get posts by their IDs."""
         return self.post_api("/posts/ids", json=ids).json()
 
     def delete_post(self, post_id: str):
+        """Delete a post."""
         return self.delete_api(f"/posts/{post_id}", throw=True).json()
